c958c4c702ada68d87fa907d3d3c9b075ed1a592
Jason Zimdars <jz@37signals.com>
2025-05-21 22:08:58 -0500

    Just use days

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -2,8 +2,8 @@ module CollectionsHelper
   def collection_auto_close_options
     [
       [ "3 days", 3.days ],
-      [ "1 week", 7.days ],
-      [ "1 month", 30.days ],
+      [ "7 days", 7.days ],
+      [ "30 days", 30.days ],
       [ "90 days", 90.days ],
       [ "365 days", 365.days ]
     ]
@@ -14,9 +14,9 @@ module CollectionsHelper
       [ "1 day", 1.days ],
       [ "2 days", 2.days ],
       [ "3 days", 3.days ],
-      [ "1 week", 7.days ],
-      [ "2 weeks", 14.days ],
-      [ "1 month", 30.days ],
+      [ "7 days", 7.days ],
+      [ "14 days", 14.days ],
+      [ "30 days", 30.days ],
       [ "365 days", 365.days ]
     ]
   end
