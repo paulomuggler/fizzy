a014af6ef08c3a49d797873712a00bca3652a61b
Jorge Manrubia <jorge@37signals.com>
2025-06-05 09:16:38 +0200

    Simplify

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,12 +1,5 @@
 module CollectionsHelper
   def collection_auto_close_options
-    [
-      [ "3", 3.days ],
-      [ "7", 7.days ],
-      [ "11", 11.days ],
-      [ "30", 30.days ],
-      [ "90", 90.days ],
-      [ "365", 365.days ]
-    ]
+    [ 3, 7, 11, 30, 90, 365 ]
   end
 end
