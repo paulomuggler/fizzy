49b63c856e223fd0418d276fa534aac88c4dc59a
Jason Zimdars <jz@37signals.com>
2025-10-29 11:04:03 -0500

    Copy edits

--- app/helpers/webhooks_helper.rb
+++ app/helpers/webhooks_helper.rb
@@ -1,17 +1,17 @@
 module WebhooksHelper
   ACTION_LABELS = {
     card_assigned: "Card assigned",
-    card_closed: "Card moved to Done",
+    card_closed: "Card moved to “Done”",
     card_postponed: "Card moved to “Not Now”",
     card_auto_postponed: "Card auto-closed as “Not Now”",
     card_collection_changed: "Card collection changed",
-    card_published: "Card published",
+    card_published: "Card added",
     card_reopened: "Card reopened",
-    card_sent_back_to_triage: "Card move back to Maybe",
+    card_sent_back_to_triage: "Card move back to “Maybe?”",
     card_title_changed: "Card title changed",
     card_triaged: "Card column changed",
     card_unassigned: "Card unassigned",
-    comment_created: "Comment created"
+    comment_created: "Comment added"
   }.with_indifferent_access.freeze
 
   def webhook_action_options(actions = Webhook::PERMITTED_ACTIONS)
